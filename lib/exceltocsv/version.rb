c50d2f2a9c6a712aa42e6d073ba136c2b9b1e74d
Jeff McAffee <jeff@ktechsystems.com>
2014-06-03 23:45:28 -0700

    Bump version to 0.2.0

--- lib/exceltocsv/version.rb
+++ lib/exceltocsv/version.rb
@@ -7,13 +7,10 @@ module ExcelToCsv
   APPNAME = "ExcelToCsv" unless constants.include?("APPNAME")
   COPYRIGHT = "Copyright (c) 2014, kTech Systems LLC. All rights reserved." unless constants.include?("COPYRIGHT")
 
-
   def self.logo()
     return  [ "#{ExcelToCsv::APPNAME} v#{ExcelToCsv::VERSION}",
               "#{ExcelToCsv::COPYRIGHT}",
               ""
             ].join("\n")
   end
-
-
 end # module ExcelToCsv
